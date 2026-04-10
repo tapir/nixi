9552ff93092b99a010e8c5455e43de1451ac55c1
Cosku Bas <cosku.bas@gmail.com>
2026-04-10 19:51:06 +0200

    merge files

--- hosts/desktop/desktop.nix
+++ hosts/desktop/desktop.nix
@@ -3,7 +3,6 @@
 {
     imports = [
         ./hardware-configuration.nix
-        ./flatpak.nix
         ../../modules/boot.nix
         ../../modules/base.nix
         ../../modules/packages.nix
@@ -15,6 +14,31 @@
     environment.systemPackages = with pkgs; [
         steam
     ];
+
+    services.flatpak = {
+        enable = true;
+        packages = [
+            "com.discordapp.Discord"
+            "com.github.tchx84.Flatseal"
+            "com.mattjakeman.ExtensionManager"
+            "com.orcaslicer.OrcaSlicer"
+            "com.ranfdev.DistroShelf"
+            "io.dbeaver.DBeaverCommunity"
+            "io.github.celluloid_player.Celluloid"
+            "net.nokyan.Resources"
+            "org.gimp.GIMP"
+            "org.gnome.Calculator"
+            "org.gnome.Firmware"
+            "org.gnome.Logs"
+            "org.gnome.Loupe"
+            "org.gnome.Papers"
+            "org.gnome.TextEditor"
+            "org.inkscape.Inkscape"
+            "org.onlyoffice.desktopeditors"
+            "org.telegram.desktop"
+            "page.tesk.Refine"
+        ];
+    };
 
     system.stateVersion = "26.05";
 }
--- hosts/laptop/laptop.nix
+++ hosts/laptop/laptop.nix
@@ -3,7 +3,6 @@
 {
     imports = [
         ./hardware-configuration.nix
-        ./flatpak.nix
         ../../modules/boot.nix
         ../../modules/base.nix
         ../../modules/packages.nix
@@ -11,6 +10,31 @@
     ];
 
     networking.hostName = "nixcosh-laptop";
+
+    environment.systemPackages = with pkgs; [ ];
+
+    services.flatpak = {
+        enable = true;
+        packages = [
+            "com.github.tchx84.Flatseal"
+            "com.mattjakeman.ExtensionManager"
+            "com.ranfdev.DistroShelf"
+            "io.dbeaver.DBeaverCommunity"
+            "io.github.celluloid_player.Celluloid"
+            "net.nokyan.Resources"
+            "org.gimp.GIMP"
+            "org.gnome.Calculator"
+            "org.gnome.Firmware"
+            "org.gnome.Logs"
+            "org.gnome.Loupe"
+            "org.gnome.Papers"
+            "org.gnome.TextEditor"
+            "org.inkscape.Inkscape"
+            "org.onlyoffice.desktopeditors"
+            "org.telegram.desktop"
+            "page.tesk.Refine"
+        ];
+    };
 
     system.stateVersion = "26.05";
 }
