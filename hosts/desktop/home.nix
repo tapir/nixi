bf695dc16abb9b21303499e3360c52272942f247
Cosku Bas <cosku.bas@gmail.com>
2026-04-12 14:42:39 +0200

    pipewire config

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -4,6 +4,12 @@
   home.homeDirectory = "/home/${config.home.username}";
   home.stateVersion = nixosConfig.system.stateVersion;
 
+  xdg.configFile."pipewire/pipewire.conf.d/99-sink-hd599-eq.conf".source =
+    ./files/99-sink-hd599-eq.conf;
+
+  xdg.configFile."wireplumber/wireplumber.conf.d/99-default-sink.conf".source =
+    ./files/99-default-sink.conf;
+
   imports = [
     ../../modules/home/fonts.nix
   ];
