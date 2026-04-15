54d259733b3da4029f0f9b03fb280fd69477d73c
Cosku Bas <cosku.bas@gmail.com>
2026-04-15 16:34:15 +0200

    remove autosuspend

--- modules/ugreen-dock.nix
+++ modules/ugreen-dock.nix
@@ -10,9 +10,5 @@
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="6c:1f:f7:75:07:ae", \
       RUN+="${pkgs.ethtool}/bin/ethtool --set-eee usbeth0 eee off"
-
-    # Disable autosuspend on the RTL8153 ethernet itself
-    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0bda", ATTR{idProduct}=="8153", \
-      ATTR{power/control}="on", ATTR{power/autosuspend}="-1"
   '';
 }
