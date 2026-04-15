805af548f4523e234cff12fb812bf4819fcd6cd0
Cosku Bas <cosku.bas@gmail.com>
2026-04-15 16:05:46 +0200

    lets try without renaming

--- modules/ugreen-dock.nix
+++ modules/ugreen-dock.nix
@@ -2,7 +2,7 @@
 {
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="6c:1f:f7:75:07:ae", \
-      RUN+="${pkgs.ethtool}/bin/ethtool --set-eee usbeth0 eee off", \
-      RUN+="${pkgs.ethtool}/bin/ethtool -K usbeth0 tso off gso off gro off"
+      RUN+="${pkgs.ethtool}/bin/ethtool --set-eee $env{INTERFACE} eee off", \
+      RUN+="${pkgs.ethtool}/bin/ethtool -K $env{INTERFACE} tso off gso off gro off"
   '';
 }
