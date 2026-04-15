7189a2cf1f86d6282930d30d947cb107a5bca290
Cosku Bas <cosku.bas@gmail.com>
2026-04-15 16:12:44 +0200

    final version

--- modules/ugreen-dock.nix
+++ modules/ugreen-dock.nix
@@ -1,8 +1,15 @@
 { pkgs, ... }:
 {
+  # Rename the dongle to usbeth0 based on MAC
+  systemd.network.links."10-usbeth0" = {
+    matchConfig.MACAddress = "6c:1f:f7:75:07:ae";
+    linkConfig.Name = "usbeth0";
+  };
+
+  # Apply ethtool tuning when the device appears
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="6c:1f:f7:75:07:ae", \
-      RUN+="${pkgs.ethtool}/bin/ethtool --set-eee $env{INTERFACE} eee off", \
-      RUN+="${pkgs.ethtool}/bin/ethtool -K $env{INTERFACE} tso off gso off gro off"
+      RUN+="${pkgs.ethtool}/bin/ethtool --set-eee usbeth0 eee off", \
+      RUN+="${pkgs.ethtool}/bin/ethtool -K usbeth0 tso off gso off gro off"
   '';
 }
