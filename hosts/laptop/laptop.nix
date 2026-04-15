85a54da7e19587e0468190c48e1c9d907c394f24
Cosku Bas <cosku.bas@gmail.com>
2026-04-15 15:12:12 +0200

    testing power manager

--- hosts/laptop/laptop.nix
+++ hosts/laptop/laptop.nix
@@ -14,10 +14,10 @@
 
   #environment.systemPackages = with pkgs; [ ];
 
-  boot.blacklistedKernelModules = [ "cdc_ether" ];
-  boot.kernelModules = [ "r8152" ];
+  # boot.blacklistedKernelModules = [ "cdc_ether" ];
+  # boot.kernelModules = [ "r8152" ];
 
-  powerManagement.enable = true;
+  # powerManagement.enable = true;
   services.logind.settings.Login = {
     SleepOperation = "suspend";
     HandlePowerKey = "suspend";
--- modules/packages.nix
+++ modules/packages.nix
@@ -65,6 +65,7 @@
       resources
       iosevka
       nixfmt
+      usbutils
     ];
   };
 
