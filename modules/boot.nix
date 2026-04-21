469a2e45b6a77213076831f67373233e92ef2f97
Cosku Bas <cosku.bas@gmail.com>
2026-04-21 09:15:11 +0200

    disable boot anim

--- modules/boot.nix
+++ modules/boot.nix
@@ -10,29 +10,29 @@
       efi.canTouchEfiVariables = true;
     };
 
-    # Boot animation
-    plymouth = {
-      enable = true;
-      theme = "sliced";
-      themePackages = with pkgs; [
-        (adi1090x-plymouth-themes.override {
-          selected_themes = [ "sliced" ];
-        })
-      ];
-    };
+    # # Boot animation
+    # plymouth = {
+    #   enable = true;
+    #   theme = "sliced";
+    #   themePackages = with pkgs; [
+    #     (adi1090x-plymouth-themes.override {
+    #       selected_themes = [ "sliced" ];
+    #     })
+    #   ];
+    # };
 
-    # Silence boot messages
-    consoleLogLevel = 3;
+    # # Silence boot messages
+    # consoleLogLevel = 3;
 
-    initrd = {
-      verbose = false;
-    };
+    # initrd = {
+    #   verbose = false;
+    # };
 
-    kernelParams = [
-      "quiet"
-      "udev.log_level=3"
-      "systemd.show_status=auto"
-    ];
+    # kernelParams = [
+    #   "quiet"
+    #   "udev.log_level=3"
+    #   "systemd.show_status=auto"
+    # ];
 
     kernelModules = [ "tcp_bbr" ];
 
