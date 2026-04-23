8c58a5f79751085cdd90d807d4241961a04a729c
Cosku Bas <cosku.bas@gmail.com>
2026-04-23 13:00:48 +0200

    firefox gnome theme home manger

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,8 +1,30 @@
-{ config, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.extraSpecialArgs = {
     nixosConfig = config;
   };
+
+  home-manager.users.cosku =
+    { pkgs, lib, ... }:
+    {
+      home.activation.firefoxGnomeTheme = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        theme_version="${pkgs.firefox-gnome-theme.version or "unknown"}"
+        stamp_dir="$HOME/.cache/firefox-gnome-theme"
+        stamp="$stamp_dir/installed-$theme_version"
+
+        if [ ! -f "$stamp" ]; then
+          ${pkgs.firefox-gnome-theme}/bin/install.sh
+          mkdir -p "$stamp_dir"
+          rm -f "$stamp_dir/installed-"*
+          touch "$stamp"
+        fi
+      '';
+    };
 }
