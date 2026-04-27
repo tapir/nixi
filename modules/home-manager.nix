bbc5ebc546d5231c04eeb4c7aad830c607c3f8bf
Cosku Bas <cosku.bas@gmail.com>
2026-04-27 19:26:23 +0200

    remove firefox gnome theme

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -10,21 +10,4 @@
   home-manager.extraSpecialArgs = {
     nixosConfig = config;
   };
-
-  home-manager.users.cosku =
-    { pkgs, lib, ... }:
-    {
-      home.activation.firefoxGnomeTheme = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        theme_version="${pkgs.firefox-gnome-theme.version or "unknown"}"
-        stamp_dir="$HOME/.cache/firefox-gnome-theme"
-        stamp="$stamp_dir/installed-$theme_version"
-
-        if [ ! -f "$stamp" ]; then
-          ${pkgs.firefox-gnome-theme}/bin/install.sh
-          mkdir -p "$stamp_dir"
-          rm -f "$stamp_dir/installed-"*
-          touch "$stamp"
-        fi
-      '';
-    };
 }
