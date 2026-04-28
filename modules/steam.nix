7d07034bb65dbd7209d0716ca91f614dac2b7400
Cosku Bas <cosku.bas@gmail.com>
2026-04-28 09:32:01 +0200

    separate steam.nix

---
+++ modules/steam.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+{
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+  };
+
+  boot.kernelModules = [ "ntsync" ];
+
+  environment.sessionVariables = {
+    PROTON_USE_NTSYNC = "1";
+  };
+}
