afaf40d5dcbc2f78e16f97c45a84c7a404e08f96
Cosku Bas <cosku.bas@gmail.com>
2026-04-29 20:32:15 +0200

    home manager backup

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -2,6 +2,7 @@
 {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
+  home-manager.backupFileExtension = "bak";
   home-manager.extraSpecialArgs = {
     nixosConfig = config;
   };
