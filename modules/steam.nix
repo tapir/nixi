a405661f91d8b849b954983d29f34da1a226fb00
Cosku Bas <cosku.bas@gmail.com>
2026-05-03 17:46:58 +0200

    x11 for old games

--- modules/steam.nix
+++ modules/steam.nix
@@ -15,7 +15,7 @@
 
   environment.sessionVariables = {
     PROTON_USE_NTSYNC = "1";
-    SDL_VIDEODRIVER = "wayland";
+    SDL_VIDEODRIVER = "x11";
     SDL_VIDEO_DRIVER = "wayland";
     SDL_AUDIODRIVER = "pipewire";
     SDL_AUDIO_DRIVER = "pipewire";
