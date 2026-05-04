cdcd7b45707a921055fae4e4681b68d20d1d53be
Cosku Bas <cosku.bas@gmail.com>
2026-05-04 13:41:51 +0200

    add st-link device

--- modules/embedded.nix
+++ modules/embedded.nix
@@ -5,6 +5,11 @@
     openocd
   ];
 
+  # ST-Link V2 adapter
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="3748", MODE="0666", GROUP="plugdev"
+  '';
+
   services.udev.packages = [ pkgs.openocd ];
   users = {
     groups.plugdev = { };
