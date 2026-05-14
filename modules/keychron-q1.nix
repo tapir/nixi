6831716d576d7121ae004eb18a22098e9aa9735b
Cosku Bas <cosku.bas@gmail.com>
2026-05-14 17:26:44 +0200

    works already

--- modules/keychron-q1.nix
+++ modules/keychron-q1.nix
@@ -3,20 +3,13 @@
 {
   environment.systemPackages = with pkgs; [
     qmk
-    qmk-udev-rules
     via
   ];
 
   hardware.keyboard.qmk.enable = true;
   services.udev = {
     packages = with pkgs; [
-      qmk
-      qmk-udev-rules
       via
     ];
   };
-
-  services.udev.extraRules = ''
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0810", MODE="0666", GROUP="users" 
-  '';
 }
