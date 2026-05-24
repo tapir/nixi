78931d4b8ec7222d973ae90f170c2ee07f865567
Cosku Bas <cosku.bas@gmail.com>
2026-05-24 16:03:40 +0200

    sunxi

--- modules/embedded.nix
+++ modules/embedded.nix
@@ -4,11 +4,13 @@
   environment.systemPackages = with pkgs; [
     openocd
     inetutils
+    sunxi-tools
   ];
 
-  # ST-Link V2 adapter
+  # ST-Link V2 adapter and sunxi-fel
   services.udev.extraRules = ''
     SUBSYSTEM=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="3748", MODE="0666", GROUP="plugdev"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="1f3a", ATTRS{idProduct}=="efe8", MODE="0666", GROUP="plugdev"
   '';
 
   services.udev.packages = [ pkgs.openocd ];
@@ -16,7 +18,6 @@
     groups.plugdev = { };
     users.cosku.extraGroups = [
       "plugdev"
-      "dialout"
     ];
   };
 }
