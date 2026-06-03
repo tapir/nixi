12555f50d8589d434ce025c02d01b8807bd2e9fd
Cosku Bas <cosku.bas@gmail.com>
2026-06-03 10:33:52 +0200

    gnome key bindings

--- modules/home/gnome.nix
+++ modules/home/gnome.nix
@@ -1,7 +1,16 @@
 { ... }:
 
 {
-  dconf.settings."org/gnome/mutter" = {
-    workspaces-only-on-primary = false;
+  dconf.settings = {
+    "org/gnome/mutter" = {
+      workspaces-only-on-primary = false;
+    };
+
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-windows = [ "<Alt>Tab" ];
+      switch-windows-backward = [ "<Shift><Alt>Tab" ];
+      switch-applications = [ "<Super>Tab" ];
+      switch-applications-backward = [ "<Shift><Super>Tab" ];
+    };
   };
 }
