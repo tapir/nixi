93c63ece9d1689e131f8305afa07d03d536576ca
Cosku Bas <cosku.bas@gmail.com>
2026-06-24 17:58:48 +0200

    overlay openblas

--- modules/base.nix
+++ modules/base.nix
@@ -170,7 +170,7 @@
     pipewire = {
       enable = true;
       alsa.enable = true;
-      #alsa.support32Bit = true;
+      alsa.support32Bit = true;
       pulse.enable = true;
       jack.enable = true;
     };
@@ -183,4 +183,17 @@
     options = "--delete-older-than 7d";
   };
   nix.settings.auto-optimise-store = true;
+
+  # temp
+  nixpkgs.overlays = [
+    (final: prev: {
+      openblas =
+        if prev.stdenv.hostPlatform.system == "i686-linux" then
+          prev.openblas.overrideAttrs (_: {
+            doCheck = false;
+          })
+        else
+          prev.openblas;
+    })
+  ];
 }
