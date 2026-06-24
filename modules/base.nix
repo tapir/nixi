00725d03f960d5cab517dc29f304afc3bdfeb270
Cosku Bas <cosku.bas@gmail.com>
2026-06-24 09:36:23 +0200

    fix flake problem

--- modules/base.nix
+++ modules/base.nix
@@ -170,7 +170,7 @@
     pipewire = {
       enable = true;
       alsa.enable = true;
-      alsa.support32Bit = true;
+      #alsa.support32Bit = true;
       pulse.enable = true;
       jack.enable = true;
     };
