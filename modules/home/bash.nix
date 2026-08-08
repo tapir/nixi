6312221f065495f573f942bb520780f63620297f
Cosku Bas <cosku.bas@gmail.com>
2026-08-08 10:03:45 +0200

    cleanup

--- modules/home/bash.nix
+++ modules/home/bash.nix
@@ -6,7 +6,7 @@
     shellAliases = {
       nixupdate = "nix flake update && git pull && git commit -am \"nixpkg update\" && git push";
       devrun = "distrobox enter arch-dev";
-      devreset = "docker pull ghcr.io/tapir/my-arch-dev:latest && yes | distrobox rm arch-dev && distrobox create --name arch-dev --image ghcr.io/tapir/my-arch-dev:latest --additional-flags \"--device=nvidia.com/gpu=all\" --volume /run/opengl-driver:/run/opengl-driver --volume /run/opengl-driver-32:/run/opengl-driver-32";
+      devreset = "docker pull ghcr.io/tapir/my-arch-dev:latest && yes | distrobox rm arch-dev && distrobox create --no-entry --name arch-dev --image ghcr.io/tapir/my-arch-dev:latest --additional-flags \"--device=nvidia.com/gpu=all\" --volume /run/opengl-driver:/run/opengl-driver --volume /run/opengl-driver-32:/run/opengl-driver-32";
     };
     initExtra = ''
       export VK_DRIVER_FILES=/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.json
--- modules/steam.nix
+++ modules/steam.nix
@@ -6,15 +6,19 @@
     gamemode
   ];
 
-  programs.steam = {
-    enable = true;
-    extraCompatPackages = [ pkgs.proton-ge-bin ];
+  programs.steam.enable = true;
+
+  services.flatpak = {
+    packages = [
+      "com.vysp3r.ProtonPlus"
+    ];
   };
 
   boot.kernelModules = [ "ntsync" ];
 
   environment.sessionVariables = {
     PROTON_USE_NTSYNC = "1";
+    PROTON_DXVK_LOWLATENCY = 1;
     SDL_AUDIODRIVER = "pipewire";
     SDL_AUDIO_DRIVER = "pipewire";
   };
