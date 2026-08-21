28b2d35cce808b645baeaa000cae098cc103a30c
Cosku Bas <cosku.bas@gmail.com>
2026-08-21 10:25:01 +0200

    disable pi for now

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -16,7 +16,7 @@
     ../../modules/home/mangohud.nix
     ../../modules/home/distrobox.nix
     ../../modules/home/bash.nix
-    ../../modules/home/pi.nix
+    #../../modules/home/pi.nix
   ];
 
   programs.bash = {
