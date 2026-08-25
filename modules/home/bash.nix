ef292081ca13af7f0f55e2615afb58056ef6e1d7
Cosku Bas <cosku.bas@gmail.com>
2026-08-25 13:37:49 +0200

    func instead of alias

--- modules/home/bash.nix
+++ modules/home/bash.nix
@@ -4,9 +4,24 @@
   programs.bash = {
     enable = true;
     shellAliases = {
-      nixupdate = "cd $HOME/Documents/nixi && git pull && nix flake update && git commit -am \"nixpkg update\" && git push && cd -";
       devrun = "distrobox enter arch-dev";
       containerclean = "docker system prune -a --volumes && podman system prune -a --volumes";
     };
+
+    # Function instead of alias: handles "nothing to commit" without
+    # aborting the chain (which would skip git push and strand you in nixi).
+    initExtra = ''
+      nixupdate() {
+        cd "$HOME/Documents/nixi" || return 1
+        git pull || return 1
+        nix flake update || return 1
+        git add -A
+        if ! git diff --cached --quiet; then
+          git commit -m "nixpkg update" || return 1
+        fi
+        git push || return 1
+        cd -
+      }
+    '';
   };
 }
