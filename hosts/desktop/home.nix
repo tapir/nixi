b4ef7a21e04c68b5823a27999693a9046d084fbb
Cosku Bas <cosku.bas@gmail.com>
2026-09-02 10:45:23 +0200

    desktop

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -27,6 +27,9 @@
     };
     initExtra = ''
       export VK_DRIVER_FILES=/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.json
+      export JAVA_HOME=/usr/lib/jvm/java-21-openjdk
+      export ANDROID_HOME=$HOME/Android/Sdk
+      export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools
     '';
   };
 }
