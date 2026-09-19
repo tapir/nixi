b4a5ba0feb7ca31cb9888c0bad650bdb66da783b
Cosku Bas <cosku.bas@gmail.com>
2026-09-19 16:08:21 +0200

    fix nvidia

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -10,9 +10,16 @@
     };
 
     nvidia = {
-      # ponytail: nixpkgs production (595.x) doesn't compile on kernel 7.2 (strncpy removed);
-      # latest (610.57.04) does. Drop this line when nixpkgs production catches up.
-      package = config.boot.kernelPackages.nvidiaPackages.latest;
+      # ponytail: effective nixpkgs production (595.71.05) doesn't compile on kernel 7.2
+      # (strncpy removed); 610.57.04 does. Replace with `branch = "latest";` once it catches up.
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "610.57.04";
+        sha256_64bit = "sha256-suk1xmuDuwDAyFe8jg7g/VLekoa0DJzB7sKafOfrEW0=";
+        sha256_aarch64 = "sha256-QCefrMBCmpOwuOyXv1k5Gj0iB2CYlPgnG3JToUw/j54=";
+        openSha256 = "sha256-rQHOOOY4KL92Ww3KDwh+j4eGU7oNAH8LutZC5wmFnPo=";
+        settingsSha256 = "sha256-ZEMo8I8Zc2Tq6RVDNYpAH+f094dUaZiBqO+5f6lIjRI=";
+        persistencedSha256 = "sha256-aXmD2VY1RLlgAnlHhOUMWzvMyhI6JTClcFLm4imF/mA=";
+      };
       open = true;
       modesetting.enable = true;
       powerManagement.enable = true;
